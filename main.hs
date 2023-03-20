8c96891193ec65492efe05b6468b3bdf566ef993
cmrxnn <cmrxn2005@gmail.com>
2023-03-20 12:44:56 +0000

    Add map list & odd/even calculator

--- main.hs
+++ main.hs
@@ -1,6 +1,21 @@
-import GHC.Float ( divideFloat )
+import Data.Map (Map) 
+import qualified Data.Map as Map
 
-main :: IO ()
-main = do
-  let c = divideFloat 15 7 :: Float
-  print c
+eveno :: Int -> Bool 
+noto  :: Bool -> String 
+myMap :: Integer -> Map Integer [Integer] 
+
+myMap n = Map.fromList (map makePair [1..n]) 
+   where makePair x = (x, [x * 2])  
+
+eveno x = if x `rem` 2 == 0 
+   then True 
+else False 
+
+noto x = if x == True 
+   then "Number is even"
+else "Number  is odd"
+
+main = do 
+   print ((noto.eveno) 10)
+   print(myMap 10)
